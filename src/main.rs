f801ee29d22a182231e3a62148ed88f3eed02699
Kriptikz <Kriptikz@users.noreply.github.com>
2024-08-07 19:33:46 -0500

    username and password websocket authentication

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{io::Read, ops::{ControlFlow, Range}, sync::Arc, time::Duration};
+use std::{io::{self, Read, Write}, ops::{ControlFlow, Range}, sync::Arc, time::Duration};
 
 use drillx::equix;
 use futures_util::{SinkExt, StreamExt};
+use rpassword::read_password;
 use tokio::{sync::{mpsc::{UnboundedReceiver, UnboundedSender}, Mutex}, time::Instant};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
+use tokio_tungstenite::{connect_async, tungstenite::{handshake::client::{generate_key, Request}, Message}};
+use base64::prelude::*;
 
 #[derive(Debug)]
 pub enum ServerMessage {
@@ -13,140 +15,169 @@ pub enum ServerMessage {
 
 #[tokio::main]
 async fn main() {
-    let url = "ws://127.0.0.1:3000/";
+    let url = url::Url::parse("wss://domainexpansion.tech/").expect("Failed to parse server url");
+    let host = url.host_str().expect("Invalid host in server url");
+
+    print!("Username: ");
+    let _ = io::stdout().flush();
+    let mut username = String::new();
+    if let Err(e) = io::stdin().read_line(&mut username) {
+        println!("Error reading username input: {:?}", e);
+    }
+    print!("Password: ");
+    let _ = io::stdout().flush();
+    let password = read_password().expect("Failed to read password");
+
+    let auth = BASE64_STANDARD.encode(format!("{}:{}", username, password));
 
     loop {
         println!("Connecting to server...");
+        let request = Request::builder()
+            .method("GET")
+            .uri(url.to_string())
+            .header("Sec-Websocket-Key", generate_key())
+            .header("Host", host)
+            .header("Upgrade", "websocket")
+            .header("Connection", "upgrade")
+            .header("Sec-Websocket-Version", "13")
+            .header("Authorization", format!("Basic {}", auth))
+            .body(())
+            .unwrap();
 
-        if let Ok((ws_stream, _)) = connect_async(url).await {
-            println!("Connected to network!");
+        match connect_async(request).await {
+            Ok((ws_stream, _)) => {
+                println!("Connected to network!");
 
-            let (mut sender, mut receiver) = ws_stream.split();
-            let (message_sender, mut message_receiver) = tokio::sync::mpsc::unbounded_channel::<ServerMessage>();
+                let (mut sender, mut receiver) = ws_stream.split();
+                let (message_sender, mut message_receiver) = tokio::sync::mpsc::unbounded_channel::<ServerMessage>();
 
-            let receiver_thread = tokio::spawn(async move {
-                while let Some(Ok(message)) = receiver.next().await {
-                    if process_message(message, message_sender.clone()).is_break() {
-                        break;
+                let receiver_thread = tokio::spawn(async move {
+                    while let Some(Ok(message)) = receiver.next().await {
+                        if process_message(message, message_sender.clone()).is_break() {
+                            break;
+                        }
                     }
-                }
-            });
+                });
 
-            // send Ready message
-            let _ = sender.send(Message::Binary(0u8.to_le_bytes().to_vec())).await;
+                // send Ready message
+                let _ = sender.send(Message::Binary(0u8.to_le_bytes().to_vec())).await;
 
-            let sender = Arc::new(Mutex::new(sender));
+                let sender = Arc::new(Mutex::new(sender));
 
-            // receive messages
-            let message_sender = sender.clone();
-            while let Some(msg) = message_receiver.recv().await {
-                match msg {
-                    ServerMessage::StartMining(challenge, nonce_range, cutoff) => {
-                        println!("Received start mining message!");
-                        println!("Mining starting...");
-                        println!("Nonce range: {} - {}", nonce_range.start, nonce_range.end);
-                        let hash_timer = Instant::now();
-                        let threads = 64;
-                        let nonces_per_thread = 10_000;
-                        let handles = (0..threads)
-                            .map(|i| {
-                                std::thread::spawn({
-                                    let mut memory = equix::SolverMemory::new();
-                                    move || {
-                                        let first_nonce = nonce_range.start + (nonces_per_thread * i);
-                                        let mut nonce = first_nonce;
-                                        let mut best_nonce = nonce;
-                                        let mut best_difficulty = 0;
-                                        let mut best_hash = drillx::Hash::default();
-                                        let mut total_hashes: u64 = 0;
-                                        loop {
-                                            // Create hash
-                                            let hashes = drillx::get_hashes_with_memory(
-                                                &mut memory,
-                                                &challenge,
-                                                &nonce.to_le_bytes()
-                                            );
+                // receive messages
+                let message_sender = sender.clone();
+                while let Some(msg) = message_receiver.recv().await {
+                    match msg {
+                        ServerMessage::StartMining(challenge, nonce_range, cutoff) => {
+                            println!("Received start mining message!");
+                            println!("Mining starting...");
+                            println!("Nonce range: {} - {}", nonce_range.start, nonce_range.end);
+                            let hash_timer = Instant::now();
+                            let threads = 1;
+                            let nonces_per_thread = 10_000;
+                            let handles = (0..threads)
+                                .map(|i| {
+                                    std::thread::spawn({
+                                        let mut memory = equix::SolverMemory::new();
+                                        move || {
+                                            let first_nonce = nonce_range.start + (nonces_per_thread * i);
+                                            let mut nonce = first_nonce;
+                                            let mut best_nonce = nonce;
+                                            let mut best_difficulty = 0;
+                                            let mut best_hash = drillx::Hash::default();
+                                            let mut total_hashes: u64 = 0;
+                                            loop {
+                                                // Create hash
+                                                let hashes = drillx::get_hashes_with_memory(
+                                                    &mut memory,
+                                                    &challenge,
+                                                    &nonce.to_le_bytes()
+                                                );
 
-                                            for hash in hashes {
-                                                total_hashes += 1;
-                                                let difficulty = hash.difficulty();
-                                                if difficulty.gt(&best_difficulty) {
-                                                        best_nonce = nonce;
-                                                        best_difficulty = difficulty;
-                                                        best_hash = hash;
+                                                for hash in hashes {
+                                                    total_hashes += 1;
+                                                    let difficulty = hash.difficulty();
+                                                    if difficulty.gt(&best_difficulty) {
+                                                            best_nonce = nonce;
+                                                            best_difficulty = difficulty;
+                                                            best_hash = hash;
+                                                    }
                                                 }
-                                            }
 
-                                            // Exit if processed nonce range
-                                            if nonce >= nonce_range.end {
-                                                break;
-                                            }
-
-                                            if nonce % 100 == 0 {
-                                                if hash_timer.elapsed().as_secs().ge(&cutoff) {
+                                                // Exit if processed nonce range
+                                                if nonce >= nonce_range.end {
                                                     break;
                                                 }
-                                            } 
+
+                                                if nonce % 100 == 0 {
+                                                    if hash_timer.elapsed().as_secs().ge(&cutoff) {
+                                                        break;
+                                                    }
+                                                } 
 
 
-                                            // Increment nonce
-                                            nonce += 1;
+                                                // Increment nonce
+                                                nonce += 1;
+                                            }
+                                            
+                                            // Return the best nonce
+                                            Some((best_nonce, best_difficulty, best_hash, total_hashes))
                                         }
-                                        
-                                        // Return the best nonce
-                                        Some((best_nonce, best_difficulty, best_hash, total_hashes))
-                                    }
+                                    })
                                 })
-                            })
-                            .collect::<Vec<_>>();
+                                .collect::<Vec<_>>();
 
-                            // Join handles and return best nonce
-                            let mut best_nonce: u64 = 0;
-                            let mut best_difficulty = 0;
-                            let mut best_hash = drillx::Hash::default();
-                            let mut total_nonces_checked = 0;
-                            for h in handles {
-                                if let Ok(Some((nonce, difficulty, hash, nonces_checked))) = h.join() {
-                                    total_nonces_checked += nonces_checked;
-                                    if difficulty > best_difficulty {
-                                        best_difficulty = difficulty;
-                                        best_nonce = nonce;
-                                        best_hash = hash;
+                                // Join handles and return best nonce
+                                let mut best_nonce: u64 = 0;
+                                let mut best_difficulty = 0;
+                                let mut best_hash = drillx::Hash::default();
+                                let mut total_nonces_checked = 0;
+                                for h in handles {
+                                    if let Ok(Some((nonce, difficulty, hash, nonces_checked))) = h.join() {
+                                        total_nonces_checked += nonces_checked;
+                                        if difficulty > best_difficulty {
+                                            best_difficulty = difficulty;
+                                            best_nonce = nonce;
+                                            best_hash = hash;
+                                        }
                                     }
                                 }
+
+                            let hash_time = hash_timer.elapsed();
+
+                            println!("Found best diff: {}", best_difficulty);
+                            println!("Processed: {}", total_nonces_checked);
+                            println!("Hash time: {:?}", hash_time);
+                            let message_type =  2u8; // 1 u8 - BestSolution Message
+                            let best_hash_bin = best_hash.d; // 16 u8
+                            let best_nonce_bin = best_nonce.to_le_bytes(); // 8 u8
+                            let mut bin_data = [0; 25];
+                            bin_data[00..1].copy_from_slice(&message_type.to_le_bytes());
+                            bin_data[01..17].copy_from_slice(&best_hash_bin);
+                            bin_data[17..25].copy_from_slice(&best_nonce_bin);
+
+                            {
+                                let mut message_sender = message_sender.lock().await;
+                                let _ = message_sender.send(Message::Binary(bin_data.to_vec())).await;
                             }
 
-                        let hash_time = hash_timer.elapsed();
-
-                        println!("Found best diff: {}", best_difficulty);
-                        println!("Processed: {}", total_nonces_checked);
-                        println!("Hash time: {:?}", hash_time);
-                        let message_type =  2u8; // 1 u8 - BestSolution Message
-                        let best_hash_bin = best_hash.d; // 16 u8
-                        let best_nonce_bin = best_nonce.to_le_bytes(); // 8 u8
-                        let mut bin_data = [0; 25];
-                        bin_data[00..1].copy_from_slice(&message_type.to_le_bytes());
-                        bin_data[01..17].copy_from_slice(&best_hash_bin);
-                        bin_data[17..25].copy_from_slice(&best_nonce_bin);
-
-                        {
-                            let mut message_sender = message_sender.lock().await;
-                            let _ = message_sender.send(Message::Binary(bin_data.to_vec())).await;
-                        }
-
-                        tokio::time::sleep(Duration::from_secs(3)).await;
-                        // send new ready message
-                        {
-                            let mut message_sender = message_sender.lock().await;
-                            let _ = message_sender.send(Message::Binary(0u8.to_le_bytes().to_vec())).await;
+                            tokio::time::sleep(Duration::from_secs(3)).await;
+                            // send new ready message
+                            {
+                                let mut message_sender = message_sender.lock().await;
+                                let _ = message_sender.send(Message::Binary(0u8.to_le_bytes().to_vec())).await;
+                            }
                         }
                     }
                 }
-            }
 
-            let _ = receiver_thread.await;
-        } else {
-            tokio::time::sleep(Duration::from_secs(3)).await;
+                let _ = receiver_thread.await;
+            }, 
+            Err(e) => {
+                println!("Error: {:?}", e);
+                tokio::time::sleep(Duration::from_secs(3)).await;
+
+            }
         }
     }
 }
