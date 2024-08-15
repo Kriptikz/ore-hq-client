aa55f65ba6b5ab978e2c70b289dafbc315a43685
ec1ipse <ec1ipsedonsol@gmail.com>
2024-08-15 01:51:46 +0000

    sign up confirm and exit condition

--- src/signup.rs
+++ src/signup.rs
@@ -1,36 +1,28 @@
 use std::str::FromStr;
 use std::io::Read;
-use spl_token::amount_to_ui_amount;
 
 use base64::{prelude::BASE64_STANDARD, Engine};
 use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer, system_instruction, transaction::Transaction};
-
+use std::io::{self, Write};
 
 pub fn ask_confirm(question: &str) -> bool {
     println!("{}", question);
     loop {
-        let mut input = [0];
-        let _ = std::io::stdin().read(&mut input);
-        match input[0] as char {
-            'y' | 'Y' => return true,
-            'n' | 'N' => return false,
+        io::stdout().flush().unwrap();  // Ensure prompt is printed before reading input
+
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+
+        match input.trim().chars().next() {
+            Some('y') | Some('Y') => return true,
+            Some('n') | Some('N') => return false,
             _ => println!("Please type only Y or N to continue."),
         }
     }
 }
 
-
 pub async fn signup(url: String, key: Keypair, unsecure: bool) {
     let base_url = url;
-
-    if !ask_confirm(
-        format!(
-            "\nYou are about to sign up to mine with Ec1ipse Mining Pool, this costs 0.001 Solana.\nWould you like to continue? [Y/n]"
-        )
-        .as_str(),
-    ) {
-        return;
-    }
 
     let client = reqwest::Client::new();
 
@@ -39,6 +31,23 @@ pub async fn signup(url: String, key: Keypair, unsecure: bool) {
     } else {
         "https".to_string()
     };
+
+    let check_url = format!("{}://{}/check-user?pubkey={}", url_prefix, base_url, key.pubkey().to_string());
+    let check_resp = client.get(check_url).send().await.unwrap().text().await.unwrap();
+
+    if check_resp == "EXISTS" {
+        println!("Already a user, please utilize the 'mine' command to begin mining!");
+        return;
+    }
+
+    if !ask_confirm(
+        format!(
+            "\nYou are about to sign up to mine with {}, this costs 0.001 Solana.\nWould you like to continue? [Y/n]",
+            base_url
+        ).as_str(),
+    ) {
+        return;
+    }
 
     let resp = client.get(format!("{}://{}/pool/authority/pubkey", url_prefix, base_url)).send().await.unwrap().text().await.unwrap();
 
@@ -66,6 +75,9 @@ pub async fn signup(url: String, key: Keypair, unsecure: bool) {
                 "SUCCESS" => {
                     println!("Successfully signed up!");
                 },
+                "EXISTS" => {
+                    println!("You're already signed up!");
+                }
                 _ => {
                     println!("Transaction failed, please wait and try again.");
                 }
