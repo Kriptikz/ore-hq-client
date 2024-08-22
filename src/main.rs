81f343115fcb12754c53925136e1418a67a41065
Ec1ipse <ec1ipsedonsol@gmail.com>
2024-08-22 00:31:06 -0500

    2.0.0-alpha update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use claim::ClaimArgs;
 use solana_sdk::signature::read_keypair_file;
 use clap::{Parser, Subcommand};
+use inquire::{Text, Confirm, Select};
+use dirs::home_dir;
+use std::path::PathBuf;
 
 use signup::signup;
 
@@ -41,7 +44,7 @@ struct Args {
     use_http: bool,
 
     #[command(subcommand)]
-    command: Commands
+    command: Option<Commands>
 }
 
 #[derive(Debug, Subcommand)]
@@ -62,35 +65,179 @@ enum Commands {
 
 // --------------------------------
 
-
 #[tokio::main]
 async fn main() {
+    loop {
+        if let Err(_) = run_menu().await {
+            println!("An error occurred, returning to the main menu...");
+        }
+    }
+}
+
+async fn run_menu() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+
+    let options = vec![
+        "Mine",
+        "ProtoMine",
+        "Sign up",
+        "Claim Rewards",
+        "Check Reward Balance",
+        "Check Wallet Balance",
+        "Help",
+        "Exit",
+    ];
+
+    println!(); // Blank line before the prompt
+
+    let selection = match &args.command {
+        Some(_) => None, // Execute the command passed as argument
+        None => Select::new("Welcome to Ore HQ Client, what would you like to do?", options).prompt().ok(),
+    };
+
+    if selection == Some("Exit") {
+        std::process::exit(0);
+    }
+
+    if selection == Some("Help") {
+        println!("This is a command line tool to interact with the ORE mining pool. Use the options provided to mine, check balances, claim rewards, or sign up.");
+        return Ok(());
+    }
 
     let base_url = args.url;
     let unsecure_conn = args.use_http;
-    let key = read_keypair_file(args.keypair.clone()).expect(&format!("Failed to load keypair from file: {}", args.keypair));
-    match args.command {
-        Commands::Mine(args) => {
-            mine::mine(args, key, base_url, unsecure_conn).await;
-        },
-        Commands::Protomine(args) => {
-            protomine::mine(args, key, base_url, unsecure_conn).await;
-        },
-        Commands::Signup => {
-            signup(base_url, key, unsecure_conn).await;
-        },
-        Commands::Claim(args) => {
-            claim::claim(args, key, base_url, unsecure_conn).await;
+
+    // Resolve the keypair path, expanding '~' to home directory
+    let keypair_path = if args.keypair.starts_with("~") {
+        if let Some(home_dir) = home_dir() {
+            let mut expanded_path = PathBuf::from(home_dir);
+            expanded_path.push(&args.keypair[2..]); // Skip '~/' in keypair path
+            expanded_path.to_string_lossy().into_owned()
+        } else {
+            args.keypair.clone()
         }
-        Commands::Rewards => {
-            rewards::rewards(key, base_url, unsecure_conn).await;
-        }
-        Commands::Balance => {
-            balance::balance(key, base_url, unsecure_conn).await;
+    } else {
+        args.keypair.clone()
+    };
+
+    // Prompt the user to confirm using the default keypair path
+    if keypair_path.ends_with("id.json") {
+        let use_default = Confirm::new(&format!("Do you want to use the default keypair located at {}?", keypair_path))
+            .prompt()
+            .unwrap_or(false);
+
+        if !use_default {
+            let custom_keypair = Text::new("Please enter the path to your keypair:")
+                .prompt()
+                .unwrap();
+            let key = read_keypair_file(&custom_keypair)
+                .expect(&format!("Failed to load keypair from file: {}", custom_keypair));
+            run_command(args.command, key, base_url, unsecure_conn, selection).await?;
+            return Ok(());
         }
     }
 
+    let key = read_keypair_file(&keypair_path)
+        .expect(&format!("Failed to load keypair from file: {}", keypair_path));
 
+    run_command(args.command, key, base_url, unsecure_conn, selection).await?;
+
+    Ok(())
 }
 
+async fn run_command(
+    command: Option<Commands>,
+    key: solana_sdk::signature::Keypair,
+    base_url: String,
+    unsecure_conn: bool,
+    selection: Option<&str>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match command {
+        Some(Commands::Mine(args)) => {
+            mine::mine(args, key, base_url, unsecure_conn).await;
+        },
+        Some(Commands::Protomine(args)) => {
+            protomine::mine(args, key, base_url, unsecure_conn).await;
+        },
+        Some(Commands::Signup) => {
+            let confirm_signup = Confirm::new("Do you want to sign up to the pool?")
+                .prompt()
+                .unwrap_or(false);
+
+            if confirm_signup {
+                signup(base_url, key, unsecure_conn).await;
+            } else {
+                println!("Sign up cancelled.");
+            }
+        },
+        Some(Commands::Claim(args)) => {
+            let confirm_claim = Confirm::new(&format!("Do you want to claim {} rewards?", args.amount))
+                .prompt()
+                .unwrap_or(false);
+
+            if confirm_claim {
+                claim::claim(args, key, base_url, unsecure_conn).await;
+            } else {
+                println!("Claim cancelled.");
+            }
+        },
+        Some(Commands::Rewards) => {
+            rewards::rewards(key, base_url, unsecure_conn).await;
+        },
+        Some(Commands::Balance) => {
+            balance::balance(key, base_url, unsecure_conn).await;
+        },
+        None => {
+            if let Some(choice) = selection {
+                match choice {
+                    "Mine" => {
+                        let cores = Text::new("Enter the number of cores:").prompt()?;
+                        let args = mine::MineArgs { cores: cores.parse()? };
+                        mine::mine(args, key, base_url, unsecure_conn).await;
+                    },
+                    "ProtoMine" => {
+                        let cores = Text::new("Enter the number of cores:").prompt()?;
+                        let args = protomine::MineArgs { cores: cores.parse()? };
+                        protomine::mine(args, key, base_url, unsecure_conn).await;
+                    },
+                    "Sign up" => {
+                        let confirm_signup = Confirm::new("Are you sure you want to sign up to the pool?")
+                            .prompt()
+                            .unwrap_or(false);
+
+                        if confirm_signup {
+                            signup(base_url, key, unsecure_conn).await;
+                        } else {
+                            println!("Sign up cancelled.");
+                        }
+                    },
+                    "Claim Rewards" => {
+                        let amount = Text::new("Enter the amount to claim:").prompt()?;
+                        let confirm_claim = Confirm::new(&format!("Are you sure you want to claim {} rewards?", amount))
+                            .prompt()
+                            .unwrap_or(false);
+
+                        if confirm_claim {
+                            let args = ClaimArgs { amount: amount.parse()? };
+                            claim::claim(args, key, base_url, unsecure_conn).await;
+                        } else {
+                            println!("Claim cancelled.");
+                        }
+                    },
+                    "Check Reward Balance" => {
+                        rewards::rewards(key, base_url, unsecure_conn).await;
+                    },
+                    "Check Wallet Balance" => {
+                        balance::balance(key, base_url, unsecure_conn).await;
+                    },
+                    "Help" => {
+                        println!("This is a command line tool to interact with the ORE mining pool. Use the options provided to mine, check balances, claim rewards, or sign up.");
+                    },
+                    _ => println!("Unknown selection."),
+                }
+            }
+        },
+    }
+
+    Ok(())
+}
