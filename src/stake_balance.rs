a1634f435ab1b0448e9f4a07f4cb126ac0dd0892
Ec1ipse <ec1ipsedonsol@gmail.com>
2024-09-04 01:11:18 -0500

    No staked account instead of 'Failed to g'.

--- src/stake_balance.rs
+++ src/stake_balance.rs
@@ -1,4 +1,5 @@
 use solana_sdk::{signature::Keypair, signer::Signer};
+use reqwest::Error;
 
 pub async fn stake_balance(key: Keypair, url: String, unsecure: bool) {
     let base_url = url;
@@ -10,6 +11,22 @@ pub async fn stake_balance(key: Keypair, url: String, unsecure: bool) {
         "https".to_string()
     };
 
-    let balance = client.get(format!("{}://{}/miner/stake?pubkey={}", url_prefix, base_url, key.pubkey().to_string())).send().await.unwrap().text().await.unwrap();
-    println!("  Delegated stake balance: {:.11} ORE", balance);
+    // Fetch balance
+    match client.get(format!("{}://{}/miner/stake?pubkey={}", url_prefix, base_url, key.pubkey().to_string()))
+        .send().await
+        {
+        Ok(response) => {
+            let balance = response.text().await.unwrap();
+            // Check if the balance failed to load
+            if balance.contains("Failed to g") {
+                println!("  Delegated stake balance: No staked account");
+            } else {
+                println!("  Delegated stake balance: {:.11} ORE", balance);
+            }
+        },
+        Err(e) => {
+            // Handle request failure
+            println!("  Error fetching stake balance: {:?}", e);
+        }
+    }
 }
