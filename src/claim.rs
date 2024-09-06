b28de11ba22682cc0bb606c74b9a842239d52491
Ec1ipse <ec1ipsedonsol@gmail.com>
2024-09-06 12:22:14 -0500

    Added note for claim without ATA.

--- src/claim.rs
+++ src/claim.rs
@@ -23,58 +23,60 @@ pub async fn claim(args: ClaimArgs, key: Keypair, url: String, unsecure: bool) {
         "https".to_string()
     };
 
-    // Fetch and display balance and rewards
     let balance_response = client
-        .get(format!(
-            "{}://{}/miner/balance?pubkey={}",
-            url_prefix,
-            url,
-            key.pubkey().to_string()
-        ))
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+    .get(format!(
+        "{}://{}/miner/balance?pubkey={}",
+        url_prefix,
+        url,
+        key.pubkey().to_string()
+    ))
+    .send()
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
 
-    let balance = balance_response.parse::<f64>().unwrap_or(0.0);
+let balance = balance_response.parse::<f64>().unwrap_or(0.0);
 
-    let rewards_response = client
-        .get(format!(
-            "{}://{}/miner/rewards?pubkey={}",
-            url_prefix,
-            url,
-            key.pubkey().to_string()
-        ))
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+let rewards_response = client
+    .get(format!(
+        "{}://{}/miner/rewards?pubkey={}",
+        url_prefix,
+        url,
+        key.pubkey().to_string()
+    ))
+    .send()
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
 
-    let rewards = rewards_response.parse::<f64>().unwrap_or(0.0);
+let rewards = rewards_response.parse::<f64>().unwrap_or(0.0);
 
-    println!("  Unclaimed Rewards: {:.11} ORE", rewards);
-    println!("  Wallet Balance:    {:.11} ORE", balance);
+println!("  Unclaimed Rewards: {:.11} ORE", rewards);
+println!("  Wallet Balance:    {:.11} ORE", balance);
 
-    if rewards < 0.005 {
-        println!();
-        println!("  You have not reached the required claim limit of 0.005 ORE.");
-        println!("  Keep mining to accumulate more rewards before you can withdraw.");
-        return;
-    }
+// Check if the wallet balance is zero and display the note
+if balance == 0.0 {
+    println!("\n  Note: A 0.004 ORE fee will be deducted from your claim amount to cover\n
+    the cost of Token Account Creation. This is a one time fee used to create the ORE Token Account.");
+}
+
+if rewards < 0.005 {
+    println!();
+    println!("  You have not reached the required claim limit of 0.005 ORE.");
+    println!("  Keep mining to accumulate more rewards before you can withdraw.");
+    return;
+}
 
     // Convert balance to grains
     let balance_grains = (rewards * 10f64.powf(ore_api::consts::TOKEN_DECIMALS as f64)) as u64;
 
-    // If balance is zero, inform the user about the ATA fee and return to keypair selection
+    // If balance is zero, inform the user and return to keypair selection
     if balance_grains == 0 {
         println!("\n  There is no balance to claim.");
-        println!(
-            "  Note: A 0.004 ORE fee will be withdrawn to cover the cost of creating an Associated Token Account (ATA) if you have not done so yet."
-        );
         return;
     }
 
@@ -135,35 +137,35 @@ pub async fn claim(args: ClaimArgs, key: Keypair, url: String, unsecure: bool) {
     }
 
     // RED TEXT
-    match Text::new(
-        &format!(
-            "  Are you sure you want to claim {} ORE? (Y/n or 'esc' to cancel)",
-            amount_to_ui_amount(claim_amount_grains, ore_api::consts::TOKEN_DECIMALS)
-        )
-        .red()
-        .to_string(),
+match Text::new(
+    &format!(
+        "  Are you sure you want to claim {} ORE? (Y/n or 'esc' to cancel)",
+        amount_to_ui_amount(claim_amount_grains, ore_api::consts::TOKEN_DECIMALS)
     )
-    .prompt()
-    {
-        Ok(confirm) => {
-            if confirm.trim().eq_ignore_ascii_case("esc") {
-                println!("  Claim canceled.");
-                return;
-            } else if confirm.trim().is_empty() || confirm.trim().to_lowercase() == "y" {
-            } else {
-                println!("  Claim canceled.");
-                return;
-            }
-        }
-        Err(InquireError::OperationCanceled) => {
-            println!("  Claim operation canceled.");
+    .red()
+    .to_string(),
+)
+.prompt()
+{
+    Ok(confirm) => {
+        if confirm.trim().eq_ignore_ascii_case("esc") {
+            println!("  Claim canceled.");
             return;
-        }
-        Err(_) => {
-            println!("  Invalid input. Claim canceled.");
+        } else if confirm.trim().is_empty() || confirm.trim().to_lowercase() == "y" {
+        } else {
+            println!("  Claim canceled.");
             return;
         }
     }
+    Err(InquireError::OperationCanceled) => {
+        println!("  Claim operation canceled.");
+        return;
+    }
+    Err(_) => {
+        println!("  Invalid input. Claim canceled.");
+        return;
+    }
+}
 
     println!(
         "  Sending claim request for {} ORE...",
