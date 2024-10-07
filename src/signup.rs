c30e8346927aa40870acaaacf1a499befdbef949
Kriptikz <Kriptikz@users.noreply.github.com>
2024-10-07 14:16:28 -0500

    remove signup transaction creation

--- src/signup.rs
+++ src/signup.rs
@@ -1,10 +1,8 @@
 use std::str::FromStr;
 
-use base64::{prelude::BASE64_STANDARD, Engine};
 use clap::Parser;
 use solana_sdk::{
-    pubkey::Pubkey, signature::Keypair, signer::Signer, system_instruction,
-    transaction::Transaction,
+    pubkey::Pubkey, signature::Keypair, signer::Signer
 };
 
 #[derive(Debug, Parser)]
@@ -24,7 +22,7 @@ pub async fn signup(args: SignupArgs, url: String, key: Keypair, unsecure: bool)
             Ok(pk) => {
                 pk
             },
-            Err(e) => {
+            Err(_e) => {
                 println!("Invalid miner pubkey arg provided.");
                 return;
             }
@@ -43,51 +41,15 @@ pub async fn signup(args: SignupArgs, url: String, key: Keypair, unsecure: bool)
         "https".to_string()
     };
 
-    let resp = client
-        .get(format!(
-            "{}://{}/pool/authority/pubkey",
-            url_prefix, base_url
-        ))
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-
-    let pool_pubkey = Pubkey::from_str(&resp).unwrap();
-
-    let resp = client
-        .get(format!("{}://{}/latest-blockhash", url_prefix, base_url))
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-
-    let decoded_blockhash = BASE64_STANDARD.decode(resp).unwrap();
-    let deserialized_blockhash = bincode::deserialize(&decoded_blockhash).unwrap();
-
-    let ix = system_instruction::transfer(&key.pubkey(), &pool_pubkey, 1_000_000);
-
-    let mut tx = Transaction::new_with_payer(&[ix], Some(&key.pubkey()));
-
-    tx.sign(&[&key], deserialized_blockhash);
-
-    let serialized_tx = bincode::serialize(&tx).unwrap();
-
-    let encoded_tx = BASE64_STANDARD.encode(&serialized_tx);
 
     let resp = client
         .post(format!(
-            "{}://{}/v2/signup?miner={}&fee_payer={}",
+            "{}://{}/v2/signup?miner={}",
             url_prefix,
             base_url,
             miner_pubkey.to_string(),
-            key.pubkey().to_string(),
         ))
-        .body(encoded_tx)
+        .body("BLANK".to_string())
         .send()
         .await;
     if let Ok(res) = resp {
