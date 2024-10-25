cd58b8cce399505a223dbfc1425236cace772d17
Kriptikz <Kriptikz@users.noreply.github.com>
2024-10-24 20:08:55 -0500

    show wallet pubkey when checking balance

--- src/balance.rs
+++ src/balance.rs
@@ -12,6 +12,8 @@ pub async fn balance(key: &Keypair, url: String, unsecure: bool) {
         "https".to_string()
     };
 
+    println!("Wallet: {}", key.pubkey().to_string());
+
     // Fetch Wallet (Stakeable) Balance
     let balance_response = client
         .get(format!(
